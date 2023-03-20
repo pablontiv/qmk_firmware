31c83ff094c62c5841e93b3dc14a86fe91f084f6
Pablo <pablo.ontiveros@gmail.com>
2023-03-20 15:10:51 -0600

    remove modifiers

--- keyboards/ymdk/ymd40/air40/keymaps/pones/keymap.c
+++ keyboards/ymdk/ymd40/air40/keymaps/pones/keymap.c
@@ -67,13 +67,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  GUI_A,    ALT_S,    CTL_D,  SFT_F,  KC_G,   KC_H,   SFT_J,  CTL_K,    ALT_L,    GUI_SCLN, KC_QUOT,
     // KC_TAB,  KC_A,    KC_S,    KC_D,  KC_F,  KC_G,   KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,  KC_V,  KC_B,   KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH, SC_SENT,
-    KC_LCTL, KC_LGUI, KC_LALT, MO(4), MO(1), LT(5,KC_SPC), KC_SPC, MO(2), MO(3),   KC_LALT, KC_LGUI, KC_LCTL
+    ___, ___, ___, MO(4), MO(1), LT(5,KC_SPC), KC_SPC, MO(2), MO(3),   ___, ___, ___
+    // KC_LCTL, KC_LGUI, KC_LALT, MO(4), MO(1), LT(5,KC_SPC), KC_SPC, MO(2), MO(3),   KC_LALT, KC_LGUI, KC_LCTL
   ),
   [_NUMBER] = LAYOUT_ortho_4x12(
     ___,   KC_F1, KC_F2,  KC_F3,  KC_F4,  ___, ___, KC_7, KC_8, KC_9,   ___, KC_BSPC,
     ___,   KC_F5, KC_F6,  KC_F7,  KC_F8,  ___, ___, KC_4, KC_5, KC_6,   ___, ___,
-    ___,   KC_F9, KC_F10, KC_F11, KC_F12, ___, ___, KC_1, KC_2, KC_3,   ___, KC_RSFT,
-    KC_WAKE, ___, ___,  ___,  ___,  ___, ___, KC_0, KC_0, KC_DOT, ___, KC_RCTL
+    ___,   KC_F9, KC_F10, KC_F11, KC_F12, ___, ___, KC_1, KC_2, KC_3,   ___, ___,
+    KC_WAKE, ___, ___,  ___,  ___,  ___, ___, KC_0, KC_0, KC_DOT, ___, ___
   ),
   [_SYMBOL] = LAYOUT_ortho_4x12(
     KC_GRV, S(KC_1),     S(KC_2), S(KC_3),     S(KC_4),     S(KC_5),  ___,       DV_ARR,   ___,      ___,   ___,    KC_DELETE,
@@ -85,7 +86,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ___, FZ_EXT, FZ_LFT, FZ_RGT, WD_CLO,  WI_CLO,   ___, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R,  KC_BTN1,
     ___, ___, ___, ___, ___, ___, ___, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_WH_U,
     ___, ___,  ___,  ___, ___, ___,   ___, KC_HOME, KC_PGDN, KC_PGUP, KC_END,   KC_WH_D,
-    ___, ___,  ___,  ___,  ___,  ___,   ___, ___,   ___,   KC_LALT, KC_LGUI,  KC_LCTL
+    ___, ___,  ___,  ___,  ___,  ___,   ___, ___,   ___,   ___, ___,  ___
   ),
   [_DEV] = LAYOUT_ortho_4x12(
     C(KC_GRV),  C(S(KC_GRV)), C(KC_PGDN), ___, ___, ___, ___, ___,     ___, ___, ___, ___,

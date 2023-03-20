6adf59ecb2000b942339682dba68d6e672374e5a
Pablo <pablo.ontiveros@gmail.com>
2023-03-20 16:57:24 -0600

    remove mod tap again

--- keyboards/ymdk/ymd40/air40/keymaps/pones/keymap.c
+++ keyboards/ymdk/ymd40/air40/keymaps/pones/keymap.c
@@ -30,17 +30,17 @@ enum layer_names {
 // #define WI_CYC A(KC_TAB)
 // #define WI_RCY A(KC_TAB)
 
-// Left-hand home row mods
-#define GUI_A LGUI_T(KC_A)
-#define ALT_S LALT_T(KC_S)
-#define CTL_D LCTL_T(KC_D)
-#define SFT_F LSFT_T(KC_F)
+// // Left-hand home row mods
+// #define GUI_A LGUI_T(KC_A)
+// #define ALT_S LALT_T(KC_S)
+// #define CTL_D LCTL_T(KC_D)
+// #define SFT_F LSFT_T(KC_F)
 
-// Right-hand home row mods
-#define SFT_J RSFT_T(KC_J)
-#define CTL_K RCTL_T(KC_K)
-#define ALT_L LALT_T(KC_L)
-#define GUI_SCLN RGUI_T(KC_SCLN)
+// // Right-hand home row mods
+// #define SFT_J RSFT_T(KC_J)
+// #define CTL_K RCTL_T(KC_K)
+// #define ALT_L LALT_T(KC_L)
+// #define GUI_SCLN RGUI_T(KC_SCLN)
 
 // Shorthand macros
 #define TAP(x) tap_code16(x)
@@ -64,11 +64,11 @@ const uint16_t PROGMEM clear_line_combo[] = {KC_BSPC, KC_LSFT, COMBO_END};
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT_ortho_4x12(
     QK_GESC, KC_Q,    KC_W,    KC_E,  KC_R,  KC_T,   KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC,
-    KC_TAB,  GUI_A,    ALT_S,    CTL_D,  SFT_F,  KC_G,   KC_H,   SFT_J,  CTL_K,    ALT_L,    GUI_SCLN, KC_QUOT,
-    // KC_TAB,  KC_A,    KC_S,    KC_D,  KC_F,  KC_G,   KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    // KC_TAB,  GUI_A,    ALT_S,    CTL_D,  SFT_F,  KC_G,   KC_H,   SFT_J,  CTL_K,    ALT_L,    GUI_SCLN, KC_QUOT,
+    KC_TAB,  KC_A,    KC_S,    KC_D,  KC_F,  KC_G,   KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,  KC_V,  KC_B,   KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH, SC_SENT,
-    ___, ___, ___, MO(4), MO(1), LT(5,KC_SPC), KC_SPC, MO(2), MO(3),   ___, ___, ___
-    // KC_LCTL, KC_LGUI, KC_LALT, MO(4), MO(1), LT(5,KC_SPC), KC_SPC, MO(2), MO(3),   KC_LALT, KC_LGUI, KC_LCTL
+    // ___, ___, ___, MO(4), MO(1), LT(5,KC_SPC), KC_SPC, MO(2), MO(3),   ___, ___, ___
+    KC_LCTL, KC_LGUI, KC_LALT, MO(4), MO(1), LT(5,KC_SPC), KC_SPC, MO(2), MO(3),   KC_LALT, KC_LGUI, KC_LCTL
   ),
   [_NUMBER] = LAYOUT_ortho_4x12(
     ___,   KC_F1, KC_F2,  KC_F3,  KC_F4,  ___, ___, KC_7, KC_8, KC_9,   ___, KC_BSPC,

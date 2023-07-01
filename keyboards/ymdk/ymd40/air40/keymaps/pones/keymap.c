4aeced91d4f934048cc3a2ba6fa106ab57e7a0c8
Pablo <pablo.ontiveros@gmail.com>
2023-06-30 23:05:15 -0600

    add crkbd pones keymap

--- keyboards/ymdk/ymd40/air40/keymaps/pones/keymap.c
+++ keyboards/ymdk/ymd40/air40/keymaps/pones/keymap.c
@@ -94,7 +94,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
   [_NUMBER] = LAYOUT_ortho_4x12(
     TO(0),    KC_F1,    KC_F2,    KC_F3,    KC_F4,    XXXXXXX,  XXXXXXX,  KC_7,     KC_8,     KC_9,     KC_EQUAL, KC_BSPC,
-    XXXXXXX,  KC_F5,    KC_F6,    KC_F7,    KC_F8,    XXXXXXX,  KC_ASTR,  KC_4,     KC_5,     KC_6,     KC_PLUS,  XXXXXXX,
+    XXXXXXX,  KC_F5,    KC_F6,    KC_F7,    KC_F8,    XXXXXXX,    ,  KC_4,     KC_5,     KC_6,     KC_PLUS,  XXXXXXX,
     XXXXXXX,  KC_F9,    KC_F10,   KC_F11,   KC_F12,   XXXXXXX,  KC_SLASH, KC_1,     KC_2,     KC_3,     KC_MINUS, KC_ENT,
     KC_WAKE,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  SPC_TE,   XXXXXXX,  KC_0,     KC_0,     KC_DOT,   XXXXXXX,  XXXXXXX
   ),
